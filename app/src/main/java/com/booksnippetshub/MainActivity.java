b0bfca587c3e9402eadbe2c7fcf260cbdc44e945
Ljqiii <ljq917181927@gmail.com>
2019-06-10 15:55:53 +0800

    tabbar ok

---
+++ app/src/main/java/com/booksnippetshub/MainActivity.java
@@ -0,0 +1,45 @@
+package com.booksnippetshub;
+
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.tabbar_discovery);
+                    return true;
+                case R.id.navigation_collection:
+                    mTextMessage.setText(R.string.tabbar_collection);
+                    return true;
+                case R.id.navigation_notification:
+                    mTextMessage.setText(R.string.tabbar_notification);
+                    return true;
+                case R.id.navigation_me:
+                    mTextMessage.setText(R.string.tabbar_me);
+                    return true;
+            }
+            return true;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        mTextMessage = findViewById(R.id.message);
+        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
